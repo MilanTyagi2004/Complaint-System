f943310c709b467ef492e281fffed11a96c56543
MilanTyagi2004 <milantyagi2004@gmail.com>
2025-09-14 16:40:17 +0530

    update user repository

--- src/main/java/com/SIH/SIH/repostitory/UserRepository.java
+++ src/main/java/com/SIH/SIH/repostitory/UserRepository.java
@@ -4,4 +4,5 @@ import com.SIH.SIH.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User,String> {
+    User findByEmail(String email);
 }
