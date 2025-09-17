a3308c8eb474376448ef7f1b8218c6a7b843fdd6
MilanTyagi2004 <milantyagi2004@gmail.com>
2025-09-17 13:34:46 +0530

    update user controller

--- src/main/java/com/SIH/SIH/controller/UserController.java
+++ src/main/java/com/SIH/SIH/controller/UserController.java
@@ -2,10 +2,82 @@ package com.SIH.SIH.controller;
 
 import com.SIH.SIH.entity.User;
 import com.SIH.SIH.repostitory.UserRepository;
+import com.SIH.SIH.services.UserService;
+import org.apache.tomcat.util.http.parser.HttpParser;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<?> deleteByEmail(){
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if(authentication.getName().equals("anonymousUser")) {
+                return new ResponseEntity<>("User Not authenticate", HttpStatus.UNAUTHORIZED);
+
+            }
+            userRepository.deleteByEmail(authentication.getName());
+            return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping("/update/email")
+    public ResponseEntity<?> updateUser(@RequestBody User user){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(authentication.getName().equals("anonymousUser")){
+            return new ResponseEntity<>("User not authenticate",HttpStatus.UNAUTHORIZED);
+        }
+        String email = authentication.getName();
+        User userIndb = userRepository.findByEmail(email);
+        if(userIndb==null){
+            return new ResponseEntity<>("User not found",HttpStatus.NOT_FOUND);
+        }
+        userIndb.setEmail(user.getEmail());
+        userService.saveUser(userIndb);
+        return new ResponseEntity<>(userIndb, HttpStatus.OK);
+    }
+    @PutMapping("update/password")
+    public ResponseEntity<?> updatePassword(@RequestBody User user){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication.getName().equals("anonymousUser")){
+            return new ResponseEntity<>("User not authenticated",HttpStatus.UNAUTHORIZED);
+        }
+        String email = authentication.getName();
+        User userInDb = userRepository.findByEmail(email);
+
+        if(userInDb ==null){
+            return new ResponseEntity<>("User not found",HttpStatus.NOT_FOUND);
+        }
+        userInDb.setPassword(user.getPassword());
+        userService.saveUserPassword(userInDb);
+        return new ResponseEntity<>(userInDb,HttpStatus.OK);
+    }
+    @PutMapping("update/password")
+    public ResponseEntity<?> updatePhoneNumber(@RequestBody User user){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication.getName().equals("anonymousUser")){
+            return new ResponseEntity<>("User not authenticated",HttpStatus.UNAUTHORIZED);
+        }
+        String email = authentication.getName();
+        User userInDb = userRepository.findByEmail(email);
+
+        if(userInDb ==null){
+            return new ResponseEntity<>("User not found",HttpStatus.NOT_FOUND);
+        }
+        userInDb.setMobileNumber(user.getMobileNumber());
+        userService.saveUser(userInDb);
+        return new ResponseEntity<>(userInDb,HttpStatus.OK);
+    }
 }
--- src/main/java/com/SIH/SIH/services/UserService.java
+++ src/main/java/com/SIH/SIH/services/UserService.java
@@ -3,6 +3,8 @@ package com.SIH.SIH.services;
 import com.SIH.SIH.entity.User;
 import com.SIH.SIH.repostitory.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,11 +12,18 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
     public User saveUser(User user) {
         if(userRepository.findByEmail(user.getEmail())!=null){
             throw new RuntimeException("email is already exists");
         }
         return userRepository.save(user);
+    }
+    public void saveUserPassword(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
     }
 
 }
